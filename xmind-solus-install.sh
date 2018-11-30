3010c6a940236678a4360d141b34258b63e74ee1
cantalupo <cantalupo@protonmail.com>
2018-11-30 11:05:06 -0300

    Update

--- xmind-solus-install.sh
+++ xmind-solus-install.sh
@@ -125,7 +125,7 @@ do
 						version="$(wget -qO- https://raw.githubusercontent.com/cantalupo555/xmind-solus-install/master/version.txt)"
 						wget https://github.com/cantalupo555/xmind-solus-install/raw/master/default128.png
 						sudo rm -rf xmind*
-						wget https://dl3.xmind.net/xmind-8-update8-linux.zip -O xmind-solus.zip&&mkdir xmind-solus/&&mv xmind-solus.zip xmind-solus/
+						wget $version -O xmind-solus.zip&&mkdir xmind-solus/&&mv xmind-solus.zip xmind-solus/
 						cd xmind-solus/&&unzip xmind-solus.zip&&sudo rm -rf xmind-solus.zip setup.sh
 						set -e
 						SCRIPT_NAME="$0"
@@ -143,10 +143,12 @@ do
 							fi
 							echo "[setup] Done."&&cd ..&&mv xmind-solus/ xmind/&&sudo mv xmind/ /
 							if [ -e ~/.local/share/applications/ ]; then
-								mv /xmind/configuration/XMind.desktop ~/.local/share/applications/&&cd ~
+								#mv /xmind/configuration/XMind.desktop ~/.local/share/applications/&&cd ~
+								echo -e "[Desktop Entry]\nVersion=1.0\nName=XMind\nGenericName=Mind Mapping\nGenericName[pt_BR]=Mapa Mental\nComment=The Most Popular Mind Mapping Software on The Planet\nComment[pt_BR]=O mais popular software de mapeamento mental no planeta\nExec=sh /xmind/xmind.sh\nIcon=/xmind/configuration/default128.png\nTerminal=false\nType=Application\nMimeType=application/x-xmind;\nStartupNotify=true\nCategories=Office;\nKeywords=map;mind;" | tee ~/.local/share/applications/xmind.desktop&&cd ~
 								else
 									mkdir ~/.local/share/applications/
-									mv /xmind/configuration/XMind.desktop ~/.local/share/applications/&&cd ~
+									#mv /xmind/configuration/XMind.desktop ~/.local/share/applications/&&cd ~
+									echo -e "[Desktop Entry]\nVersion=1.0\nName=XMind\nGenericName=Mind Mapping\nGenericName[pt_BR]=Mapa Mental\nComment=The Most Popular Mind Mapping Software on The Planet\nComment[pt_BR]=O mais popular software de mapeamento mental no planeta\nExec=sh /xmind/xmind.sh\nIcon=/xmind/configuration/default128.png\nTerminal=false\nType=Application\nMimeType=application/x-xmind;\nStartupNotify=true\nCategories=Office;\nKeywords=map;mind;" | tee ~/.local/share/applications/xmind.desktop&&cd ~
 								fi
 					fi
 				clear
